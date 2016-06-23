822df7acacd317f5c5834f8d18e5ff37d2bfdeca
Tim Wright <timwright12@gmail.com>
2016-06-23 11:22:08 -0400

    added HX to RGB function and cleaned up templates

--- corkscrew/assets/js/corkscrew.js
+++ corkscrew/assets/js/corkscrew.js
@@ -25,6 +25,25 @@
 
   };
 
+  // Convert a hax code to an RGB color value
+  Corkscrew.hexToRgb = function ( hex ) {
+
+    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+        return r + r + g + g + b + b;
+    });
+
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16),
+        all: parseInt(result[1], 16) + ',' + parseInt(result[2], 16) + ',' + parseInt(result[3], 16)
+    } : null;
+
+  }; // hexToRgb()
+
   Corkscrew.generateSlug = function( baseName ) {
 
     var slug = baseName + Math.floor(Math.random() * (9999 - 1)) + 1;
